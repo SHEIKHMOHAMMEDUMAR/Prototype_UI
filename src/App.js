876d0d048990504a72f72d5fef2f75ff89272a52
SHEIKH MOHAMMED UMAR <mohdumarfarooq123@gmail.com>
2022-03-29 15:26:26 +0530

    Implemented Topbar and Sodebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./styles.css";
 import { useState } from "react";
 import Drop from "./drop";
-
+import Sidebar from "./components/sidebar/Sidebar"
+import Topbar from "./components/topbar/Topbar"
 let App=() =>{
   const [selected, setselected] = useState("");
   return (
     <div className="App">
+      <Sidebar />
+      <Topbar />
       <Drop selected={selected} setselected={setselected} />
     </div>
   );
