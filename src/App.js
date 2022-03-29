e6cb660d2f88be32d5522db65b4be8c9775df497
Jatin <sjatin1308@gmail.com>
2022-03-29 17:52:44 +0530

    side+top+filter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./styles.css";
 import { useState } from "react";
+import Topbar from "./components/topbar/Topbar"
+import Sidebar from "./components/sidebar/Sidebar"
 import Drop from "./drop";
-import {userData} from "./data"
+//import {userData} from "./data"
 
 let App=() =>{
   const [selected, setselected] = useState("");
   return (
     <div className="App">
+      <Topbar />
       <Drop selected={selected} setselected={setselected} />
+      <Sidebar />
     </div>
   );
 }
