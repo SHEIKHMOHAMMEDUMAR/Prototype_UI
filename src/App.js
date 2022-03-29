d78d55ce19d241b55b5475c199f2d303372083a0
Jatin <sjatin1308@gmail.com>
2022-03-29 13:43:29 +0530

    Dropdown added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useState } from "react";
+import Drop from "./drop";
 
-function App() {
+let App=() =>{
+  const [selected, setselected] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Drop selected={selected} setselected={setselected} />
     </div>
   );
 }
-
 export default App;
